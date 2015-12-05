55a2fc76c77288082d6dbb9ed34522db20663fea
kazuhisa.oomuro <kazuhisa.oomuro@gmail.com>
2015-12-05 20:48:17 +0900

    test

--- test01.js
+++ test01.js
@@ -1,1 +1,17 @@
 
+var TARGET_URL = "https://ja.wikipedia.org/";
+
+var app = require('app');
+var BrowserWindow = require('browser-window');
+
+var mainWindow = null;
+
+app.on('ready', function(){
+
+    // メインウィンドウを作成
+    mainWindow = new BrowserWindow({width:800, height:600});
+
+    // 指定のURLを読み込み
+    mainWindow.loadUrl(TARGET_URL);
+});
+
